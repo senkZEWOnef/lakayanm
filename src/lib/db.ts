e026debe97bca6639e1d87f4babd66aafef4c369
Ralph Ulysse <ralphulysse@Ralphs-MacBook-Air.local>
2025-10-28 21:29:13 -0400

    fixed

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,26 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    datasources: { db: { url: process.env.DATABASE_URL } },
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+let prismaClient: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaNeon(neon);
+  
+  prismaClient = new PrismaClient({
+    adapter,
+    log: ["error"],
   });
+} else {
+  prismaClient = new PrismaClient({
+    datasources: { db: { url: process.env.DATABASE_URL } },
+    log: ["query", "error", "warn"],
+  });
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClient;
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
