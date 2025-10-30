3fbb85c25176ac3e99e7a09cdfdef4b0b18ceb83
Ralph Ulysse <ralphulysse@Ralphs-MacBook-Air.local>
2025-10-30 19:30:54 -0400

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media', // Enable dark mode based on system preference
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
